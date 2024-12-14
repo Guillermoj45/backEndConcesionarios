fec5671c786a5a6109f303c70484986d643a9eb6
guill <guillermoremesa@gmail.com>
2024-12-14 20:36:43 +0100

    DTO

---
+++ src/main/java/back/End/Concesionario/DTO/RegisterDTO.java
@@ -0,0 +1,12 @@
+package back.End.Concesionario.DTO;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class RegisterDTO {
+    private String username;
+    private String password;
+    private String email;
+
+}
